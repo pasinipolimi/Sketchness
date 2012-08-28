25f59d3a24ec5fa95c8810e1b309ad99e19923ee
Luca Galli <lgalli@elet.polimi.it>
2012-08-28 15:08:41 +0200

    Initial push of the game
    First version of the game with chat and canvas drawings working

---
+++ app/controllers/Application.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.libs.F.*;
+
+import org.codehaus.jackson.*;
+
+import views.html.*;
+
+import models.*;
+
+public class Application extends Controller {
+  
+	static PaintRoom env = new PaintRoom("Public");
+  
+    /**
+     * Display the home page.
+     */
+    public static Result index() {
+        return ok(index.render());
+    }
+  
+    /**
+     * Display the chat room.
+     */
+    public static Result chatRoom(String username) {
+        if(username == null || username.trim().equals("")) {
+            flash("error", "Please choose a valid username.");
+            return redirect(routes.Application.index());
+        }
+        return ok(chatRoom.render(username,"Drawer",env));
+    }
+    
+    /**
+     * Handle the chat websocket.
+     */
+    public static WebSocket<JsonNode> chat(final String username) {
+        return new WebSocket<JsonNode>() {
+            
+            // Called when the Websocket Handshake is done.
+            public void onReady(WebSocket.In<JsonNode> in, WebSocket.Out<JsonNode> out){
+                
+                // Join the chat room.
+                try { 
+                    ChatRoom.join(username, in, out);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+    }
+	
+	public static WebSocket<JsonNode> stream() {
+        
+        return new WebSocket<JsonNode>() {
+            @Override
+            public void onReady(In<JsonNode> in, Out<JsonNode> out) {
+                try{
+                    env.createPainter(in, out);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    
+    }
+  
+}
---
+++ app/models/PaintRoom.java
@@ -0,0 +1,95 @@
+package models;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+import play.Logger;
+import play.libs.F;
+import play.libs.Json;
+import play.mvc.WebSocket;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PaintRoom {
+	
+	public String name;
+    // The list of all connected painters (identified by ids)
+    public Map<Integer, Painter> painters = new ConcurrentHashMap<Integer, models.Painter>();
+    public AtomicInteger counter = new AtomicInteger(0);
+    public AtomicInteger connections = new AtomicInteger(0);
+
+	public PaintRoom(String name) {
+		this.name = name;
+	}
+
+    public void createPainter(final WebSocket.In<JsonNode> in, final WebSocket.Out<JsonNode> out) {
+        counter.incrementAndGet();
+        connections.incrementAndGet();
+        final int pid = counter.intValue(); // the painter id
+
+        // in: handle messages from the painter
+        in.onMessage(new F.Callback<JsonNode>() {
+            @Override
+            public void invoke(JsonNode json) throws Throwable {
+                String type = json.get("type").getTextValue();
+
+                // The painter wants to change some of his property
+                if("change".equals(type)) {
+                    Painter painter = painters.get(pid);
+                    if(painter == null) {
+                        painter = new Painter(out);
+                        painters.put(pid, painter);
+
+                        // Inform the painter who he is (which pid, he can them identify himself)
+                        ObjectNode self = Json.newObject();
+                        self.put("type", "youAre");
+                        self.put("pid", pid);
+                        painter.channel.write(self);
+
+                        // Inform the list of other painters
+                        for(Map.Entry<Integer, Painter> entry : painters.entrySet()) {
+                            ObjectNode other = (ObjectNode)entry.getValue().toJson();
+                            other.put("pid", entry.getKey());
+                            painter.channel.write(other);
+                        }
+                    }
+                    painter.updateFromJson(json);
+                }
+
+                ObjectNode node = ((ObjectNode)json);
+                node.put("pid", pid);
+                PaintRoom.this.notifyAll(node);
+            }
+        });
+
+        // User has disconnected.
+        in.onClose(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                painters.remove(pid);
+                connections.decrementAndGet();
+
+                ObjectNode json = Json.newObject();
+                json.put("type", "disconnect");
+                json.put("pid", pid);
+
+                PaintRoom.this.notifyAll(json);
+
+                Logger.debug("(pid:"+pid+") disconnected.");
+                Logger.info(connections+" painter(s) currently connected.");
+            }
+        });
+
+        Logger.debug("(pid:"+pid+") connected.");
+        Logger.info(connections+" painter(s) currently connected.");
+    }
+
+    public void notifyAll(JsonNode json) {
+        for(Painter painter : painters.values()) {
+            painter.channel.write(json);
+        }
+    }
+	
+}
