5cb3865a2e7ce974ff53c269accf99539cd46ad5
Druta Tim <deathosirislife@yahoo.com>
2012-11-07 11:31:51 +0100

    facebook login

--- application/app/controllers/Application.java
+++ application/app/controllers/Application.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package controllers;
 
 import play.mvc.*;
@@ -63,11 +64,79 @@ public class Application extends Controller {
         };
     
     }
-	
-	/**
-     * Test page
-     */
-    public static Result test() {
-    	return ok(login.render());
-    }
+
+    
+
 }
+=======
+package controllers;
+
+
+import java.net.URLEncoder;
+
+import play.mvc.*;
+
+import org.codehaus.jackson.*;
+
+import views.html.*;
+
+import models.*;
+
+public class Application extends Controller {
+  
+    static PaintRoom env = new PaintRoom("Public");
+  
+    /**
+     * Display the home page.
+     */
+    public static Result index() {
+        return ok(index.render());
+    }
+    
+    /**
+     * Display the chat room.
+     */
+    public static Result chatRoom(String username) {
+        if(username == null || username.trim().equals("")) {
+            flash("error", "Please choose a valid username.");
+            return redirect(routes.Application.index());
+        }
+        return ok(chatRoom.render(username,env));
+    }
+    
+    /**
+     * Handle the chat websocket.
+     */
+    public static WebSocket<JsonNode> chat(final String username) {
+        return new WebSocket<JsonNode>() {
+            
+            // Called when the Websocket Handshake is done.
+            public void onReady(WebSocket.In<JsonNode> in, WebSocket.Out<JsonNode> out){
+                
+                // Join the chat room.
+                try { 
+                    ChatRoom.join(username, in, out, env);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+    }
+	
+	public static WebSocket<JsonNode> stream() {
+        
+        return new WebSocket<JsonNode>() {
+            @Override
+            public void onReady(In<JsonNode> in, Out<JsonNode> out) {
+                try{
+                    env.createPainter(in, out);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    
+    }
+	
+}
+>>>>>>> 7eb3311855b9540afb2e17d86b47c1d5ad6cb4b2
--- application/app/models/Player.java
+++ application/app/models/Player.java
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+
+package models;
+
+
+
+public class Player {
+    private int points;
+    private int warningsReceived;
+
+    public Player() {
+        points=0;
+        warningsReceived=0;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public int getWarningsReceived() {
+        return warningsReceived;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setWarningsReceived(int warningsReceived) {
+        this.warningsReceived = warningsReceived;
+    }
+    
+
+}
+=======
 
 package models;
 
@@ -32,7 +71,6 @@ public class Player {
         this.warningsReceived = warningsReceived;
     }
     
-    public int FBLogin(){
-    	return 2;
-    }
+    
 }
+>>>>>>> 7eb3311855b9540afb2e17d86b47c1d5ad6cb4b2
