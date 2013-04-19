7b05b2ddc57925c0e639e42fdae16399ec950c45
Dippi <dippi@altervista.org>
2013-04-19 22:05:45 +0200

    Add resources handling with Scala SBT.
    Handle the non versioned resources with a new tasks
    for the command line build tool, configurable in Build.scala file.
    Ignore a lot of files.
    Add dependecy in Build.scala.
    Add set up instrucrion in README.md.

--- project/Build.scala
+++ project/Build.scala
@@ -4,20 +4,69 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Sketchness"
-  val appVersion      = "1.0-SNAPSHOT"
+	val appName         = "Sketchness"
+	val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean,
-	"org.json" % "json" % "20090211"
-  )
+	val appDependencies = Seq(
+		// Add your project dependencies here,
+		javaCore,
+		javaJdbc,
+		javaEbean,
+		"org.json" % "json" % "20090211",
+		"net.coobird" % "thumbnailator" % "0.4.3"
+	)
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    /** Defines a new setting key that contains the resources list */
+    lazy val sketchnessResources = SettingKey[Seq[Resource]](
+		"sketchness-resources",
+		"The remote resources handled outside del VCS.")
 
+    val sketchnessResourcesSetting = sketchnessResources := Seq(
+		// Add here the required resources
+		// Resource(name: String, path :String, url :String = null, zipped :Boolean = true)
+		Resource("Results Folder", "results"),
+		Resource("Lib Folder", "lib"),
+		Resource("jQuery", "public/javascripts/jquery-1.7.1.min.js", "http://code.jquery.com/jquery-1.7.1.min.js", false),
+		Resource("jQuery i18n", "public/javascripts/jquery.i18n.properties-min-1.0.9.js", "http://jquery-i18n-properties.googlecode.com/files/jquery.i18n.properties-min-1.0.9.js", false),
+		Resource("jScrollPane", "public/javascripts/jquery.jscrollpane.min.js", "http://jscrollpane.kelvinluck.com/script/jquery.jscrollpane.min.js", false),
+		Resource("jScrollPane MouseWheel", "public/javascripts/jquery.mousewheel.js", "http://jscrollpane.kelvinluck.com/script/jquery.mousewheel.js", false),
+		Resource("jScrollPane MouseWheelIntent", "public/javascripts/mwheelIntent.js", "http://jscrollpane.kelvinluck.com/script/mwheelIntent.js", false),
+		Resource("jScrollPane CSS", "public/stylesheets/jquery.jscrollpane.css", "http://jscrollpane.kelvinluck.com/style/jquery.jscrollpane.css", false),
+		Resource("Bootstrap CSS", "public/stylesheets/bootstrap.css", "https://raw.github.com/twitter/bootstrap/1c7c5f750fc221ee94e435e4e49bc2ba1a6be5e6/bootstrap.css", false),
+		Resource("Website Images", "public/images", "http://ubuntuone.com/6TzTau0tmLx38sUSTi8WBa")
+    )
+
+    /** Defines a new task key for retrieving all the resources
+	  *
+      * (Callable from the play console in the terminal
+      * with the command "sketchness-resources-get")
+      */
+    lazy val sketchnessGetResources = TaskKey[Unit](
+		"sketchness-resources-get",
+		"Task to retrieve the remote resources handled outside del VCS.")
+
+    val sketchnessGetResourcesTask = sketchnessGetResources <<= (sketchnessResources, streams) map {
+		(resources, streams) => resources foreach { _.get(streams.log) }
+	}
+
+    /** Defines a new task key for deleting all the resources
+      *
+      * (Callable from the play console in the terminal
+      * with the command "sketchness-resources-clean")
+      */
+    lazy val sketchnessCleanResources = TaskKey[Unit](
+		"sketchness-resources-clean",
+		"Task to delete the local copy of the resources handled outside del VCS.")
+
+    val sketchnessCleanResourcesTask = sketchnessCleanResources <<= (sketchnessResources, streams) map {
+		(resources, streams) => resources foreach { _.clean(streams.log) }
+	}
+
+	val main = play.Project(appName, appVersion, appDependencies).settings(
+		// Add your own project settings here
+		sketchnessResourcesSetting,
+		sketchnessGetResourcesTask,
+		sketchnessCleanResourcesTask
+	)
 
 }
---
+++ project/Resource.scala
@@ -0,0 +1,92 @@
+import sbt._
+
+import java.net.URL
+import java.io.File
+import java.io.IOException
+
+/** Resource handler
+  *
+  * Downloads and eventually unzips a resource or simply
+  * creates a directory, deletes them when required.
+  *
+  * @constructor Hidden, use factory pattern with the companion object.
+  *
+  * @param name A descriptive name for the resource
+  * @param path The local path of the resource
+  * @param url The (valid) remote url of the resource, if null only local dir is created
+  * @param zipped Flag to mark if the resource is zipped (and has to be unzipped)
+  */
+class Resource private (name: String, path :String, url :String = null, zipped :Boolean = true) {
+
+	private val _path :File = new File(path)
+	private val _url :URL = if(url != null) new URL(url) else null
+
+	/** Retrieves the resource
+	  *
+	  * Acts only if the resource doesn't already exist
+	  * to force: delete dir manually o with [[Resource.clean]]
+	  *
+	  * @param log The logger to send message to
+	  */
+	def get(log :Logger = ConsoleLogger()) = {
+		log.info("Retrieving " + name + ".")
+		if(!_path.exists()) {
+			try {
+				if(_url == null) _path.mkdirs()
+				else if(zipped) IO.unzipURL(_url, _path)
+				else IO.download(_url, _path)
+				log.success("-> Resource retieved.")
+			} catch {
+				case e: Exception => {
+					log.error("-> Unable to get resource!")
+					e.printStackTrace()
+				}
+			}
+		} else {
+			log.info("-> Resource already existent.")
+		}
+	}
+
+	/** Delete the resource
+	  *
+	  * WARNING: Be careful, this will phisically remove the resource files
+	  *
+	  * @param log The logger to send message to
+	  */
+	def clean(log :Logger = ConsoleLogger()) = {
+		log.info("Deleting " + name + ".")
+		if(_path.exists()) {
+			try {
+				delete(_path)
+				log.success("-> Resource deleted.")
+			} catch {
+				case e :IOException => {
+					log.error("-> Unable to delete resource!")
+					e.printStackTrace()
+				}
+			}
+		} else {
+			log.info("-> Resource already non existent.")
+		}
+	}
+
+	/** Recursively deletes folder tree
+	  *
+	  * @param file The file or the folder to delete
+	  */
+	private def delete(file :File) = {
+		if(file.isDirectory()) file.listFiles() foreach { delete(_) }
+		file.delete();
+	}
+
+	override def toString() = "Resource(name: \"" + name + "\", path: \"" + path + "\", url: \"" + url + "\")"
+}
+
+/** Factory for [[Resource]] instances. */
+object Resource {
+
+	/** Creates a new resouce
+	  * @see [[Reource]] constructor
+	  */
+	def apply(name :String, path :String, url :String = null, zipped :Boolean = true) = new Resource(name, path, url, zipped)
+}
