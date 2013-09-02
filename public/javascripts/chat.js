7c8df9241ff7f1e07812e28b78e9258dd793c383
Luca Galli <lgalli@elet.polimi.it>
2013-09-02 14:56:44 +0200

    Trying to fix the requirejs merging issues

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,85 +1,5 @@
 require(["Communicator", "jquery", "i18n", "jscrollpane"], function(Communicator, $) {
 
-<<<<<<< HEAD
-	// Init jScrollPane
-	$("#messages").jScrollPane({
-		showArrows: true,
-		mantainPosition: true,
-		stickToBottom:true,
-		animateScroll: true,
-		horizontalGutter: 10
-	});
-	
-	communicator.on("system", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("join", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("quit", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("talk", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("talkNear", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("talkWarning", function(e, message) {
-		sendMessage(message);
-	});
-	
-	communicator.on("talkError", function(e, message) {
-		sendMessage(message);
-	});
-	
-	var sendMessage=function(data)
-	{
-		// Create the message element
-		var el = $('<div class="message"><span></span><p></p></div>');
-		$("span", el).text(data.user);
-		$("p", el).text(data.message);
-		$(el).addClass(data.type);
-		if (data.user === $('#currentNickname').text()) $(el).addClass('me');
-
-		var paneApi = $("#messages").data('jsp');
-		paneApi.getContentPane().append(el);
-		paneApi.reinitialise();
-		paneApi.scrollToBottom();
-	};
-	
-	
-	communicator.on("membersChange", function(e, data) {
-		var $list = $("#unorderedUserList");
-		// Update the members list
-		var userCounter = 0;
-		if($list.length>0)
-		{
-			$list.empty();
-			$(data.members).each(function()
-			{
-				var el = $('<li class="avatarIcon">'+this+'</li>');
-				$list.append(el);
-			});
-		}
-		//Deal with a custom way of handling the userlist
-		else
-		{
-			$(data.members).each(function() {
-				if (this != $('#currentNickname').text()) {
-					userCounter++;
-					$("#opponent" + userCounter).attr('name',this);
-					$("#opponent" + userCounter).attr('class',"opponentAvatar");
-				}
-			});
-		}	
-	});
-=======
 	$(function() {
 
 		// Show error message
@@ -149,7 +69,6 @@ require(["Communicator", "jquery", "i18n", "jscrollpane"], function(Communicator
 				});
 			}
 		});
->>>>>>> 5a4282b06d9deda7713e0757376e4363a7f4449a
 
 
 		// Init text area placeholder
