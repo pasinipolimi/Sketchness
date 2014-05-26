0e4c226ade601159eeb38655488d0625a6206aa0
Luca Galli <lgalli@elet.polimi.it>
2014-05-26 18:46:42 +0100

    Fixed some wrong import in adminUI, added proper collection for the CMS

--- project/Build.scala
+++ project/Build.scala
@@ -52,6 +52,7 @@ object ApplicationBuild extends Build {
 		Resource("noUISlider CSS", "public/stylesheets/lib/nouislider.css", "https://raw.github.com/leongersen/noUiSlider/master/jquery.nouislider.css", false),
 		Resource("Spectrum", "public/javascripts/lib/spectrum.js", "https://raw.github.com/bgrins/spectrum/master/spectrum.js", false),
 		Resource("Spectrum JS", "public/stylesheets/lib/spectrum.css", "https://raw.github.com/bgrins/spectrum/master/spectrum.css", false),
+		Resource("Modernizr", "public/javascripts/lib/modernizr.custom.js", "https://raw.githubusercontent.com/codrops/Blueprint-ResponsiveFullWidthGrid/master/js/modernizr.custom.js", false),
 		//Zipped files with assets: images and sounds
 		Resource("Website Images", "public/images", "http://54.228.220.100/spritesheets.zip",true),
 		Resource("Website Sound Effects", "public/sounds/effects", "http://54.228.220.100/effects.zip",true)
