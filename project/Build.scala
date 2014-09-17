89536b4fe77e2476c869860614313d6e211cdd3e
Luca Galli <lgalli@elet.polimi.it>
2014-09-17 14:06:54 +0100

    Fixed library download again

--- project/Build.scala
+++ project/Build.scala
@@ -54,7 +54,7 @@ object ApplicationBuild extends Build {
 		Resource("Spectrum JS", "public/stylesheets/lib/spectrum.css", "https://raw.github.com/bgrins/spectrum/master/spectrum.css", false),
 		Resource("Modernizr", "public/javascripts/lib/modernizr.custom.js", "https://raw.githubusercontent.com/codrops/Blueprint-ResponsiveFullWidthGrid/master/js/modernizr.custom.js", false),
 		//Zipped files with assets: images and sounds
-		Resource("Libraries", "/", "http://54.75.252.197/storage/libs.zip", true),
+		Resource("Libraries", "lib/", "http://54.75.252.197/storage/libs.zip", true),
 		Resource("Website Images", "public/images", "http://54.75.252.197/storage/spritesheets.zip",true),
 		Resource("Website Sound Effects", "public/sounds/effects", "http://54.75.252.197/storage/effects.zip",true)
 	)
