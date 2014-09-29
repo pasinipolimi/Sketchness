bb4350bbb23722e9a182d1359a2bc667c61d3438
Luca Galli <lgalli@elet.polimi.it>
2014-09-29 15:46:49 +0100

    Fixed linking of the account

--- app/models/LinkedAccount.java
+++ app/models/LinkedAccount.java
@@ -1,11 +1,11 @@
 package models;
 
 import com.feth.play.module.pa.user.AuthUser;
-import play.db.ebean.Model;
-
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import play.db.ebean.Model;
 
 @Entity
 public class LinkedAccount extends Model {
@@ -28,8 +28,11 @@ public class LinkedAccount extends Model {
 			Long.class, LinkedAccount.class);
 
 	public static LinkedAccount findByProviderKey(final User user, String key) {
-		return find.where().eq("user", user).eq("providerKey", key)
-				.findUnique();
+                List<LinkedAccount> returned = find.where().eq("user", user).eq("providerKey", key).findList();
+                if(returned.size()>0)
+                    return returned.get(returned.size()-1);
+                else
+                    return null;
 	}
 
 	public static LinkedAccount create(final AuthUser authUser) {
